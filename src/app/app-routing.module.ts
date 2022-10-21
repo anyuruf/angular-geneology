d79fdeb117c5786aabfcdc9dc35319e6401fd21b
anyuruf <anyuruf@gmail.com>
2022-10-21 12:40:20 -0400

    Feat: configure dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard-component', component: DashboardComponent },
+  { path: '',   redirectTo: '/dashboard-component', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

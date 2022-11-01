4778294e4e35a1a07ccc5b815306caccfe19e495
anyuruf <anyuruf@gmail.com>
2022-11-01 12:08:13 -0400

    Feat: scafold home page for d3

---
+++ src/app/components/info-card/info-card.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-card',
+  templateUrl: './info-card.component.html',
+  styleUrls: ['./info-card.component.sass']
+})
+export class InfoCardComponent {}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { InfoCardComponent } from 'src/app/components/info-card/info-card.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, InfoCardComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatCardModule
+  ],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
